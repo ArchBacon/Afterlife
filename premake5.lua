1a98f33e197a40d77b17c19a12515faf0d15eb38
ArchBacon <c.delicaat@hotmail.com>
2022-02-09 13:42:32 +0100

    Added SDL libs

--- premake5.lua
+++ premake5.lua
@@ -26,11 +26,35 @@ project "Afterlife"
     includedirs {
         "%{prj.name}/Engine/Public",
         "%{prj.name}/Source/Public",
+        "ThirdParty/SDL2/SDL2/include",
+        "ThirdParty/SDL2/SDL2_image/include",
+        "ThirdParty/SDL2/SDL2_ttf/include",
+    }
+
+    libdirs {
+        "ThirdParty/SDL2/SDL2/lib",
+        "ThirdParty/SDL2/SDL2_image/lib",
+        "ThirdParty/SDL2/SDL2_ttf/lib",
+    }
+
+    links {
+        "SDL2.lib",
+        "SDL2main.lib",
+        "SDL2_image.lib",
+        "SDL2_ttf.lib",
     }
 
     filter "system:windows"
         cppdialect "C++20"
         systemversion "latest"
+
+        postbuildcommands {
+            ("{COPY} ../ThirdParty/SDL2/SDL2/lib/SDL2.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2/SDL2_Image/lib/SDL2_image.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2/SDL2_Image/lib/libpng16-16.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2/SDL2_Image/lib/zlib1.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+            ("{COPY} ../ThirdParty/SDL2/SDL2_ttf/lib/SDL2_ttf.dll ../Binaries/" .. outputdir .. "/Afterlife"),
+        }
     
     filter "configurations:Debug"
         defines { "AE_DEBUG" }
