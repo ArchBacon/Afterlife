e260c762ad5963a310b0b9681cad47b6db6a71e4
ArchBacon <C.Delicaat@hotmail.com>
2022-03-12 18:57:10 +0100

    Added comments

--- Afterlife/main.cpp
+++ Afterlife/main.cpp
@@ -4,6 +4,7 @@
 #include "Timer.h"
 #include "Levels/IntroLevel.h"
 
+/** Target framerate */
 constexpr int SCREEN_FPS = 60;
 constexpr int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
 
@@ -22,6 +23,7 @@ int main(int argc, char** argv)
     {
         capTimer.Start();
 
+        /** Handle events if there are any */
         while (SDL_PollEvent(&event) != 0)
         {
             app->OnEvent(event);
@@ -33,6 +35,9 @@ int main(int argc, char** argv)
         app->Tick(deltaTime);
         app->Render();
 
+        /**
+         * If current framerate is higher then target framerate, delay until average frame time is met
+         */
         const int frameTicks = capTimer.GetTicks();
         if (frameTicks < SCREEN_TICK_PER_FRAME)
         {
